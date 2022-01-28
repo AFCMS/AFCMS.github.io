3083dc31caa425615a7ec032bbf65e2bc91b0377
AFCMS <afcm.contact@gmail.com>
2022-01-28 16:41:14 +0100

    fixes

--- src/LinkEntry.js
+++ src/LinkEntry.js
@@ -1,6 +1,6 @@
 const LinkEntry = ({icon, link, stitle, title}) => {
 	return (
-		<a title={title} href={link}>
+		<a title={title} href={link} target="_blank" rel="noopener noreferrer">
 			<div className="link_entry">
 				{icon}
 				<p className="text-lg">{stitle}</p>
