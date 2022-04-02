229b4e6a260110308dade282a6d02e2550226a8f
AFCMS <afcm.contact@gmail.com>
2022-04-02 11:03:16 +0200

    use react 18 API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const container = document.getElementById("root");
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
