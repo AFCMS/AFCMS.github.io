0359998073f85ec16dde27f226808f94658319a1
AFCMS <afcm.contact@gmail.com>
2022-04-13 17:56:22 +0200

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,41 @@
 module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: module,
+	},
 	rules: {
 		indent: "off",
 	},
+	plugins: ["@typescript-eslint"],
 	overrides: [
 		{
-			files: ["*-test.js", "*.spec.js"],
-			rules: {
-				"no-unused-expressions": "off",
+			files: ["*.json"],
+			extends: ["plugin:jsonc/recommended-with-json"],
+			parser: "jsonc-eslint-parser",
+			parserOptions: {
+				jsonSyntax: "JSONC",
+			},
+		},
+		{
+			files: ["*.jsonc"],
+			extends: ["plugin:jsonc/recommended-with-jsonc"],
+			parser: "jsonc-eslint-parser",
+			parserOptions: {
+				jsonSyntax: "JSON",
+			},
+		},
+		{
+			files: ["*.json5"],
+			extends: ["plugin:jsonc/recommended-with-json5"],
+			parser: "jsonc-eslint-parser",
+			parserOptions: {
+				jsonSyntax: "JSON5",
 			},
 		},
 	],
