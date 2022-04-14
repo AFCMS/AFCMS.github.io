5fced6b1621d5d9f1b83fe1ebc3dd7da42564009
AFCMS <afcm.contact@gmail.com>
2022-04-14 21:11:33 +0200

    render app in strict mode

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ const container = document.getElementById("root");
 
 const root = ReactDOMClient.createRoot(container);
 
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
