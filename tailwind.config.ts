8788cbfcda59ab4d90cbf32aec74c4e0f87443dc
AFCMS <afcm.contact@gmail.com>
2023-10-03 11:25:06 +0200

    Migrate TailwindCSS config file to TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
 	mode: "jit",
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
@@ -10,4 +12,4 @@ module.exports = {
 			visibility: ["group-hover"],
 		},
 	},
-};
+} satisfies Config;
