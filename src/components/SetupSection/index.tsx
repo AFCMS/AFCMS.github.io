3ff9df712b23b28631a3cc181ef26d2ca7204237
AFCMS <afcm.contact@gmail.com>
2024-01-18 17:21:29 +0100

    Update setup

--- src/components/SetupSection/index.tsx
+++ src/components/SetupSection/index.tsx
@@ -1,14 +1,7 @@
 import { Tab } from "@headlessui/react";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import { SiFedora, SiWindows11 } from "react-icons/si";
-import {
-	MdCameraAlt,
-	MdKeyboard,
-	MdLaptop,
-	MdMouse,
-	MdPhoneAndroid,
-	MdTablet,
-} from "react-icons/md";
+import { MdCameraAlt, MdHeadsetMic, MdKeyboard, MdLaptop, MdMouse, MdPhoneAndroid, MdTablet } from "react-icons/md";
 
 import Section from "../Section";
 import ButtonLink from "../ButtonLink";
@@ -57,16 +50,9 @@ const SetupSection = () => {
 							<div className="mb-4 flex flex-col gap-4">
 								<SetupComponent
 									icon={MdLaptop}
-									store={
-										"https://www.asus.com/laptops/for-gaming/tuf-gaming/asus-tuf-gaming-a15-2023/"
-									}
+									store={"https://www.asus.com/laptops/for-gaming/tuf-gaming/asus-tuf-gaming-a15-2023/"}
 									title="ASUS TUF Gaming A15"
-									description_elements={[
-										"Ryzen 7 7735HS",
-										"16GB RAM",
-										"Nvidia RTX 4060 8Go",
-										"1.5To SSD",
-									]}
+									description_elements={["Ryzen 7 7735HS", "16GB RAM", "Nvidia RTX 4060 8Go", "1.5To SSD"]}
 								/>
 								<SetupComponent
 									icon={MdPhoneAndroid}
@@ -84,11 +70,15 @@ const SetupSection = () => {
 										"https://www.corsair.com/ww/en/p/keyboards/ch-910d019-na/k60-rgb-pro-mechanical-gaming-keyboard-100-cherry-mv-mechanical-keyswitches-black-ch-910d019-na"
 									}
 									title="Corsair K60 RGB PRO"
-									description_elements={[
-										"Mecanical CHERRY MV switches",
-										"Full Size",
-										"Anti-Ghosting",
-									]}
+									description_elements={["Mecanical CHERRY MV switches", "Full Size", "Anti-Ghosting"]}
+								/>
+								<SetupComponent
+									icon={MdMouse}
+									store={
+										"https://www.corsair.com/ww/en/p/gaming-mouse/ch-9319411-eu2/m65-rgb-ultra-wireless-tunable-fps-gaming-mouse-eu-ch-9319411-eu2"
+									}
+									title="Corsair M65 RGB Ultra Wireless"
+									description_elements={["25.000 DPI", "110g"]}
 								/>
 								<SetupComponent
 									icon={MdMouse}
@@ -99,24 +89,24 @@ const SetupSection = () => {
 									description_elements={["18.000 DPI", "73g"]}
 								/>
 								<SetupComponent
-									icon={MdCameraAlt}
+									icon={MdHeadsetMic}
 									store={
-										"https://logitech.com/en-us/products/webcams/streamcam.960-001286.html"
+										"https://www.corsair.com/ww/en/p/gaming-headsets/ca-9011185-na/virtuoso-rgb-wireless-high-fidelity-gaming-headset-carbon-ca-9011185-na"
 									}
+									title="Corsair Virtuoso RGB Wireless"
+									description_elements={["Hi-Fi Audio", "Broadcast grade microphone"]}
+								/>
+								<SetupComponent
+									icon={MdCameraAlt}
+									store={"https://logitech.com/en-us/products/webcams/streamcam.960-001286.html"}
 									title="Logitech Streamcam"
 									description_elements={["USB-C", "1080p 60FPS"]}
 								/>
 								<SetupComponent
 									icon={MdTablet}
-									store={
-										"https://estore.wacom.com/en-us/wacom-intuos-m-bluetooth-black-us-ctl6100wlk0.html"
-									}
+									store={"https://estore.wacom.com/en-us/wacom-intuos-m-bluetooth-black-us-ctl6100wlk0.html"}
 									title="Wacom Intuos M (Wired)"
-									description_elements={[
-										"USB-A",
-										"4096 presure levels",
-										"GNOME integration",
-									]}
+									description_elements={["USB-A", "4096 presure levels", "GNOME integration"]}
 								/>
 							</div>
 						</Tab.Panel>
@@ -124,47 +114,29 @@ const SetupSection = () => {
 							<div className={"mb-4 flex flex-col gap-4"}>
 								<div className="flex flex-row gap-4 p-2">
 									<div className="border-r border-r-slate-400 pr-4">
-										<SiFedora
-											color={"#51A2DA"}
-											className="mb-4 w-full text-center text-6xl"
-										/>
-										<ButtonLink
-											href="https://fedoraproject.org"
-											text="Website"
-										/>
+										<SiFedora color={"#51A2DA"} className="mb-4 w-full text-center text-6xl" />
+										<ButtonLink href="https://fedoraproject.org" text="Website" />
 									</div>
 									<div className="">
-										<h3 className="text-left font-bold text-blue-600">
-											{"Fedora 39"}
-										</h3>
+										<h3 className="text-left font-bold text-blue-600">{"Fedora 39"}</h3>
 										<p className={"max-w-2xl text-left"}>
-											I currently use Fedora Workstation with the GNOME desktop
-											environment as my daily driver Linux distro. I previously
-											used Ubuntu, but I switched to get faster updates and a
-											more vanilla GNOME experience.
+											I currently use Fedora Workstation with the GNOME desktop environment as my daily driver Linux
+											distro. I previously used Ubuntu, but I switched to get faster updates and a more vanilla GNOME
+											experience.
 										</p>
 									</div>
 								</div>
 								<div className="flex flex-row gap-4 p-2">
 									<div className="border-r border-r-slate-400 pr-4">
-										<SiWindows11
-											color={"#0078D4"}
-											className="mb-4 w-full text-center text-6xl"
-										/>
-										<ButtonLink
-											href="https://www.microsoft.com/en-us/windows"
-											text="Website"
-										/>
+										<SiWindows11 color={"#0078D4"} className="mb-4 w-full text-center text-6xl" />
+										<ButtonLink href="https://www.microsoft.com/en-us/windows" text="Website" />
 									</div>
 									<div className="">
-										<h3 className="text-left font-bold text-blue-600">
-											{"Windows 11 Pro"}
-										</h3>
+										<h3 className="text-left font-bold text-blue-600">{"Windows 11 Pro"}</h3>
 										<ul className="list-disc text-left"></ul>
 										<p className={"max-w-2xl text-left"}>
-											I also have a Windows 11 Pro dual-boot I use for Windows
-											specific software like Microsoft Office, Unreal Engine and
-											some video games.
+											I also have a Windows 11 Pro dual-boot I use for Windows specific software like Microsoft Office,
+											Unreal Engine and some video games.
 										</p>
 									</div>
 								</div>
@@ -174,9 +146,7 @@ const SetupSection = () => {
 					<div className={"relative"}>
 						<span
 							className={"text absolute bottom-3 right-0"}
-							title={
-								"All links are non-affiliated.\nI only advertize products because I like them."
-							}
+							title={"All links are non-affiliated.\nI only advertize products because I like them."}
 						>
 							<InformationCircleIcon className={"h-6 w-6 text-blue-600"} />
 						</span>
