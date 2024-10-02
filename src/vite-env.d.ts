16dc656d15e61b2f6210162802beacc4e0555c1a
AFCMS <afcm.contact@gmail.com>
2024-10-02 15:03:15 +0200

    Fixes to Vite env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/react" />
 
 /**
  * This is a hack to make vite-imagetools work with typescript.
  */
 declare module "*&imagetools" {
 	const out;
+	// noinspection JSUnusedGlobalSymbols
 	export default out;
 }
