4e56c05802b3b695af4149e420e06da12687c309
AFCMS <afcm.contact@gmail.com>
2025-02-17 22:50:26 +0100

    Add projects and contact infos

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col">
 			<About />
 			<Experience />
+			<Projects />
+			<Contact />
 		</div>
 	);
 }
