1b79cd75a6eac4cb55e4ddfc06e77cd9169c23e6
AFCMS <afcm.contact@gmail.com>
2025-02-17 22:48:20 +0100

    More versatile GridCard

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -2,28 +2,28 @@
 
 interface GridCardProps {
 	class?: string;
+	class2?: string;
 	children: React.ReactNode;
 	bgImage?: string;
+	bgTransparency?: number;
 }
 
 export default function GridCard(props: GridCardProps) {
 	return (
-		<div
-			class={
-				"relative row-span-1 flex h-full w-full flex-col space-y-4 rounded-lg" + (props.class ? " " + props.class : "")
-			}
-			style={
-				props.bgImage
-					? {
-							backgroundImage: `url("${props.bgImage}")`,
-							backgroundOrigin: "center",
-							backgroundSize: "cover",
-							backgroundRepeat: "no-repeat",
-						}
-					: undefined
-			}
-		>
-			<div class={"glass-effect flex h-full w-full flex-col p-4"}>{props.children}</div>
+		<div class={"relative z-10 h-full w-full space-y-4 rounded-lg" + (props.class ? " " + props.class : "")}>
+			{props.bgImage ? (
+				<div
+					class="absolute top-0 right-0 bottom-0 left-0 -z-10 h-full w-full rounded-lg bg-cover bg-no-repeat"
+					style={{
+						backgroundImage: `url("${props.bgImage}")`,
+						backgroundOrigin: "center",
+						opacity: props.bgTransparency ? props.bgTransparency : 0.8,
+					}}
+				></div>
+			) : null}
+			<div class={"glass-effect flex h-full w-full p-4" + (props.class2 ? " " + props.class2 : "")}>
+				{props.children}
+			</div>
 		</div>
 	);
 }
