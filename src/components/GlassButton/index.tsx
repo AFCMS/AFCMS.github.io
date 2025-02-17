867ce3c231405b79395dcc1898383850d1f3b11f
AFCMS <afcm.contact@gmail.com>
2025-02-17 22:45:58 +0100

    More versatile GlassButton

--- src/components/GlassButton/index.tsx
+++ src/components/GlassButton/index.tsx
@@ -2,16 +2,26 @@ import { IconType } from "react-icons/lib";
 
 interface GlassButtonProps {
 	href: string;
-	title: string;
+	title?: string;
+	label?: string;
 	icon: IconType;
+	class?: string;
 }
 
 export default function GlassButton(props: GlassButtonProps) {
 	const { icon: Icon } = props;
 	return (
-		<a href={props.href} class="glass-effect flex flex-row items-center gap-1 p-1 px-2">
+		<a
+			href={props.href}
+			aria-label={props.label}
+			class={
+				"glass-effect flex w-auto flex-row items-center gap-1 p-1" +
+				(props.title ? " px-2" : " ") +
+				(props.class ? " " + props.class : "")
+			}
+		>
 			<Icon />
-			<span>{props.title}</span>
+			{props.title ? <span>{props.title}</span> : null}
 		</a>
 	);
 }
