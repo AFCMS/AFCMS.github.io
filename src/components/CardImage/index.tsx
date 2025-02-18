44ff5f99b0120892ae95d331feefbb33de6c8c61
AFCMS <afcm.contact@gmail.com>
2025-02-18 12:58:57 +0100

    switch from border to outline to respect images size

--- src/components/CardImage/index.tsx
+++ src/components/CardImage/index.tsx
@@ -8,7 +8,7 @@ export default function CardImage(props: CardImageProps) {
 	return (
 		<img
 			class={
-				"rounded-lg border border-indigo-300/20 bg-indigo-300/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]" +
+				"rounded-lg bg-indigo-300/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] outline outline-indigo-300/20" +
 				(props.class ? " " + props.class : "")
 			}
 			alt={props.alt}
