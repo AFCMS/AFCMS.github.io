358dbbce854253d2bda6c5acfa9c115867e47957
AFCMS <afcm.contact@gmail.com>
2025-09-20 16:36:31 +0200

    Add Fluorite to projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,6 +3,7 @@ import CardImage from "../CardImage";
 import GlassButton from "../GlassButton";
 import GridCard from "../GridCard";
 
+import picture_fluorite from "../../assets/project_fluorite.png?aspect=16:9&w=384&imagetools";
 import picture_malley from "../../assets/project_malley.png?aspect=16:9&w=384&imagetools";
 import picture_devpet from "../../assets/project_devpet.jpg?aspect=16:9&w=384&imagetools";
 import picture_subway_miner from "../../assets/project_subway_miner.png?aspect=16:9&w=384&imagetools";
@@ -12,6 +13,33 @@ export default function Projects() {
 		<section id="projects" class="text-header mx-auto mb-4 flex w-full max-w-4xl flex-col gap-4">
 			<div class="border-t-2 border-t-indigo-950"></div>
 			<h2 class="text-left text-lg font-semibold">Featured projects</h2>
+			<GridCard
+				bgImage={picture_fluorite}
+				bgTransparency={0.3}
+				class="relative flex flex-row items-center justify-center md:flex-col"
+				class2="md:flex-row flex-col md:min-h-[250px]"
+			>
+				<div class="my-auto flex grow flex-col">
+					<h2 class="mb-3 font-medium">
+						Fluorite <span class="text-header/80 font-normal">- 2025</span>
+					</h2>
+					<p>A web based PWA video player.</p>
+					<p>Features local and offline support, system integration.</p>
+				</div>
+				<CardImage class="hidden aspect-[16/9] w-96 md:block" alt="Fluorite" src={picture_fluorite} />
+				<ul class="bottom-0 left-0 mt-4 flex flex-row flex-wrap gap-2 md:absolute md:m-4">
+					<li class="glass-effect-level-2 px-1 py-0.5 text-sm">PWA</li>
+					<li class="glass-effect-level-2 px-1 py-0.5 text-sm">React</li>
+					<li class="glass-effect-level-2 px-1 py-0.5 text-sm">TailwindCSS</li>
+					<li class="glass-effect-level-2 px-1 py-0.5 text-sm">Vercel</li>
+				</ul>
+				<GlassButton
+					class="absolute top-0 right-0 m-4 md:right-auto md:left-0"
+					href="https://github.com/AFCMS/fluorite"
+					label="GitHub Repository"
+					icon={SiGithub}
+				/>
+			</GridCard>
 			<GridCard
 				bgImage={picture_malley}
 				bgTransparency={0.3}
