0945ced6127809585c4db2b0c4326e63988d736d
AFCMS <afcm.contact@gmail.com>
2025-10-21 22:44:56 +0200

    Add priority hints for logo
    Useless but add a new tech in Wappalizer lol

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,7 +8,7 @@ export default function About() {
 	return (
 		<section class="text-header font-inter relative m-auto w-full max-w-4xl px-4 py-10 lg:py-14">
 			<div class="flex flex-row items-center gap-2">
-				<img class="h-16 w-16 rounded-full shadow" src="/favicon.svg" alt="AFCMS' logo" />
+				<img class="h-16 w-16 rounded-full shadow" src="/favicon.svg" alt="AFCMS' logo" fetchPriority={"high"} />
 				<h1 class="font-inter text-header text-2xl font-bold">AFCMS</h1>
 				<span class="grow"></span>
 				<ul class="flex flex-row items-center gap-2">
